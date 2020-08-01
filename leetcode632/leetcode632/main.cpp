4d6f38fae5637e525c151d9fcd73997bcb0cd902
黄创 <>
2020-08-01 16:42:11 +0800

    简化LeetCode632，最小区间

--- leetcode632/leetcode632/main.cpp
+++ leetcode632/leetcode632/main.cpp
@@ -10,30 +10,29 @@
 #include <vector>
 using namespace std;
 
-
-
-vector<int> merge(vector<vector<int>>& nums,vector<vector<int>> &index)
+//归并排序的合并过程
+vector<int> smallestRange(vector<vector<int>>& nums)
 {
-    vector<int> result;
+    if(nums.empty())
+        return {};
+    vector<int> result={0,INT_MAX};
     int n=nums.size();
-    vector<vector<int>> ptr(n);
+    vector<int> index(n,0);
+    vector<int> includeNum;
     for(int i=0;i<n;i++)
-    {
-        ptr[i].push_back(0);
-        ptr[i].push_back(nums[i].size());
-    }
+        includeNum.push_back(nums[i][0]);
     while(true)
     {
         vector<int> temp={-1};
         for(int j=0;j<n;j++)
         {
-            if(ptr[j][0]<ptr[j][1])
+            if(index[j]<nums[j].size())
             {
                 if(temp[0]==-1)
                     temp[0]=j;
-                else if(nums[j][ptr[j][0]]==nums[temp[0]][ptr[temp[0]][0]])
+                else if(nums[j][index[j]]==nums[temp[0]][index[temp[0]]])
                     temp.push_back(j);
-                else if(nums[j][ptr[j][0]]<nums[temp[0]][ptr[temp[0]][0]])
+                else if(nums[j][index[j]]<nums[temp[0]][index[temp[0]]])
                     temp={j};
             }
         }
@@ -41,61 +40,25 @@ vector<int> merge(vector<vector<int>>& nums,vector<vector<int>> &index)
             break;
         else
         {
-            index.push_back(temp);
-            result.push_back(nums[temp[0]][ptr[temp[0]][0]]);
             for(auto t:temp)
-                ptr[t][0]++;
+                includeNum[t]=nums[t][index[t]];
+            int left=includeNum[0],right=includeNum[0];
+            for(int i=0;i<n;i++)
+            {
+                if(includeNum[i]<left)
+                    left=includeNum[i];
+                else if(includeNum[i]>right)
+                    right=includeNum[i];
+            }
+            if(right-left<result[1]-result[0])
+            {
+                result[0]=left;
+                result[1]=right;
+            }
+            for(auto t:temp)
+                index[t]++;
         }
     }
-    return result;
-}
-
-vector<int> smallestRange(vector<vector<int>>& nums)
-{
-    if(nums.empty())
-        return {};
-    vector<int> result(2,0);
-    vector<vector<int>> index;
-    auto newNums=merge(nums,index);
-    vector<int> ptr(nums.size(),-1);
-    int left=newNums[0],right=newNums.back();
-    bool valid=false;
-    for(int i=0;i<index.size();i++)
-    {
-        for(auto t:index[i])
-            ptr[t]=i;
-        if(!valid)
-        {
-            valid=true;
-            for(auto t:ptr)
-            {
-                if(t==-1)
-                {
-                    valid=false;
-                    break;
-                }
-            }
-        }
-        if(valid)
-        {
-            int min=ptr[0],max=ptr[0];
-            for(auto t:ptr)
-            {
-                if(t<min)
-                    min=t;
-                else if(t>max)
-                    max=t;
-            }
-            if(newNums[max]-newNums[min]<right-left)
-            {
-                left=newNums[min];
-                right=newNums[max];
-            }
-        }
-
-    }
-    result[0]=left;
-    result[1]=right;
     return result;
 }
 
