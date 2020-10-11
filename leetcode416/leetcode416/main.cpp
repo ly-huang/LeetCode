c7c89acbfde6a384bcfa2abec90537da25e7131d
黄创 <>
2020-10-11 15:35:09 +0800

    上传LeetCode416，分割等和子集

---
+++ leetcode416/leetcode416/main.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  leetcode416
+//
+//  Created by ly on 2020/10/11.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = int(nums.size());
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (auto& num : nums) {
+            sum += num;
+            if(num>maxNum)
+                maxNum=num;
+        }
+        if (sum & 1) {
+            return false;
+        }
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
+        }
+        vector<int> dp(target + 1, 0);
+        dp[0] = true;
+        for (int i = 0; i < n; i++)
+        {
+            int num = nums[i];
+            for (int j = target; j >= num; --j)
+            {
+                dp[j] |= dp[j - num];
+            }
+        }
+        return dp[target];
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    vector<int> nums={4,4,4,4,4,4,4,4,8,8,8,8,8,8,8,8,12,12,12,12,12,12,12,12,16,16,16,16,16,16,16,16,20,20,20,20,20,20,20,20,24,24,24,24,24,24,24,24,28,28,28,28,28,28,28,28,32,32,32,32,32,32,32,32,36,36,36,36,36,36,36,36,40,40,40,40,40,40,40,40,44,44,44,44,44,44,44,44,48,48,48,48,48,48,48,48,52,52,52,52,52,52,52,52,56,56,56,56,56,56,56,56,60,60,60,60,60,60,60,60,64,64,64,64,64,64,64,64,68,68,68,68,68,68,68,68,72,72,72,72,72,72,72,72,76,76,76,76,76,76,76,76,80,80,80,80,80,80,80,80,84,84,84,84,84,84,84,84,88,88,88,88,88,88,88,88,92,92,92,92,92,92,92,92,96,96,96,96,96,96,96,96,97,99};
+    Solution s;
+    cout<<s.canPartition(nums)<<endl;
+    return 0;
+}
